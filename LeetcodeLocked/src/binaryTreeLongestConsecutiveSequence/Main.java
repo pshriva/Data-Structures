fae05ece443103d486ac279b8d02f7a903edb385
pshriva <shrivastavapranjali@gmail.com>
2017-11-21 16:34:09 -0800

    Update Main.java

--- LeetcodeLocked/src/binaryTreeLongestConsecutiveSequence/Main.java
+++ LeetcodeLocked/src/binaryTreeLongestConsecutiveSequence/Main.java
@@ -1,21 +1,31 @@
 package binaryTreeLongestConsecutiveSequence;
 
 public class Main {
-	public static void main(String args[]){
-		TreeNode root = new TreeNode(2);
-		root.right = new TreeNode(3);
-		root.right.left = new TreeNode(2);
-		root.right.left.left = new TreeNode(1);
-		int sequenceLength = getSequenceLength(root, 1, -1);
-		System.out.println(sequenceLength);
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(6);
+		root.right = new TreeNode(9);
+		root.right.left = new TreeNode(7);
+		root.right.right = new TreeNode(10);
+		root.right.right.right = new TreeNode(11);
+		root.right.right.right.left = new TreeNode(12);
+		root.right.right.right.right = new TreeNode(12);
+		root.right.right.right.right.left = new TreeNode(13);
+		int l = getLongestSequence(root);
+		System.out.println(l);
 	}
 
-	private static int getSequenceLength(TreeNode root, int length, int parent) {
+	private static int getLongestSequence(TreeNode node) {
 		// TODO Auto-generated method stub
-		if(root == null){return length;}
-		length = (parent - root.val == 1)?length+1 : 1;
-		int left = getSequenceLength(root.left, length+1, root.val);
-		int right = getSequenceLength(root.right, length+1, root.val);
+		if(node == null){return 0;}
+		return getLongestSequence(node, 1, 0);
+	}
+
+	private static int getLongestSequence(TreeNode node, int length, int parent) {
+		// TODO Auto-generated method stub
+		if(node == null){return length-1;}
+		length = (node.val - parent == 1)?length : 1;
+		int left = getLongestSequence(node.left, length+1, node.val);
+		int right = getLongestSequence(node.right, length+1, node.val);
 		return Math.max(length, Math.max(left, right));
 	}
 }
